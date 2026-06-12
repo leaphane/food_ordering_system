48cadc8fa4f03e80b8f594987264fb81a5989d42
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2026-06-12 13:36:00 +0200

    chore: added exception class

--- food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/config/ApplicationConfig.java
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.jumpstart.food_ordering_system.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//manually define objects Spring should manage
 public class ApplicationConfig {
 
 
---
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
@@ -0,0 +1,14 @@
+package com.jumpstart.food_ordering_system.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+//custom exceptions because the built-in Java exceptions are usually too generic and don’t clearly
+// describe your application’s business rules.
+public class CategoryNotFoundException extends RuntimeException {
+
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
