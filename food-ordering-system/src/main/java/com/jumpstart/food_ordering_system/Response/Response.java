13442a6ba8d1732db2cdc8940f15fa6c7e8f854e
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2026-07-01 01:54:17 +0200

    feat: Response wrapper class methods

--- food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/Response/Response.java
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/Response/Response.java
@@ -1,16 +1,34 @@
 package com.jumpstart.food_ordering_system.Response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Response<T> {
 
     private int statusCode;
     private T data;
+    @Builder.Default
     private final LocalDateTime TIMESTAMP = LocalDateTime.now();
     private String message;
 
+
+    public static <T> Response<T> success(String message, T data) {
+        return Response.<T>builder()
+                .statusCode(200).message(message).data(data).build();
+    }
+
+    public static <T> Response<T> error(int code, String message) {
+        return Response.<T>builder()
+                .statusCode(code).message(message).build();
+    }
 
 
 
