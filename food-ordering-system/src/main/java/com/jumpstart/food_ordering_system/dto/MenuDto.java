857eb6e9629478d1670d04d3928d8b92812ac0e1
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2026-07-01 20:56:47 +0200

    feat(menu): MenuDto with validation

---
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/dto/MenuDto.java
@@ -0,0 +1,33 @@
+package com.jumpstart.food_ordering_system.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+@AllArgsConstructor
+@Data
+public class MenuDto {
+
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    @NotNull(message = "Category ID is required")
+    private Long categoryId; // input only
+
+    private String categoryName; // output only
+
+}
