b31c386ac5e7c44d290d9cc9fe632a5c954136fc
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2026-07-02 06:02:53 +0200

    feat(menu): MenuDto with validation

---
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/service/MenuService.java
@@ -0,0 +1,8 @@
+package com.jumpstart.food_ordering_system.service;
+
+public class MenuService {
+
+//    Response<MenuDto> createMenu(MenuDto dto);
+//    Response<List<MenuDto>> getAllMenus();
+//    Response<MenuDto> getMenuById(Long id);
+}
